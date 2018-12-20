9be768a7b757db73b95a8fcf5815b7867d9869ec
Fythios <AresGerard@gmail.com>
2018-12-20 00:20:33 -0800

    UI Creation
    Implimented a UI using PyQt5 using Qt designer.

--- CharGen/CharGen.py
+++ CharGen/CharGen.py
@@ -1,1 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from ui_main import Ui_mainWindow
 
+app = QApplication(sys.argv)
+window = QMainWindow()
+ui = Ui_mainWindow()
+ui.setupUi(window)
+
+window.show()
+sys.exit(app.exec_())
